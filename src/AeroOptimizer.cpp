007de263a6307a360de8b31e18003585c68d5d80
Siim Alas <siim.alas@outlook.com>
2021-03-22 13:48:35 +0200

    Updated simulation parameters

--- src/AeroOptimizer.cpp
+++ src/AeroOptimizer.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-	double requiredCma = -1;
-	double cref = 1; // 0.2;
+	double requiredCma = -1.2;
+	double cref = 0.2;
 	double sref = 0.31;
 
 	AeroOptimizer::Aerodynamics::Aerofoil* naca2421 = 
@@ -14,12 +14,12 @@ int main()
 
 	AeroOptimizer::Aerodynamics::Wing* foreWing =
 		new AeroOptimizer::Aerodynamics::Wing(naca2421, 2 * M_PI / 180, 8, 0.13);
-	foreWing->mass = 0.13 + 0.4;
+	foreWing->mass = 0.7;
 	AeroOptimizer::Aerodynamics::Aircraft::AddWing(foreWing);
 
 	AeroOptimizer::Aerodynamics::Wing* aftWing =
 		new AeroOptimizer::Aerodynamics::Wing(naca2421, 0.75 * M_PI / 180, 8, 0.18);
-	aftWing->mass = 0.18;
+	aftWing->mass = 0.5;
 	aftWing->r[2] = 0.13;
 	AeroOptimizer::Aerodynamics::Aircraft::AddWing(aftWing);
 
